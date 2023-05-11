10f9555dabdd427041a8309b36cc56f56082c45b
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-11 08:52:30 +0000

    Test react-boostrap by adding button in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>
-        Welcome to BarBelles!!
+      <Button variant="primary">Primary</Button>
       </h1>
     </div>
   );
