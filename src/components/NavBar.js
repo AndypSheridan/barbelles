affb824981ded8dd4e85f06bce393f9a6cbe2c0a
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-11 09:49:57 +0000

    Add text-left for collapsed text

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,10 +14,10 @@ const NavBar = () => {
                 <Navbar.Brand><img src={logo} alt="logo" /> BarBelles</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
+                    <Nav className="ml-auto text-left">
                         <Nav.Link><i className="fas fa-home"></i> Home</Nav.Link>
                         <Nav.Link><i className="fa-solid fa-arrow-right-to-bracket"></i> Sign in</Nav.Link>
-                        <Nav.Link><i className="fa-solid fa-plus"></i>Sign up</Nav.Link>
+                        <Nav.Link><i className="fa-solid fa-plus"></i> Sign up</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
