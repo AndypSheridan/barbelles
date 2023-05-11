210821313bc50417cc97defeb2eb2b5bd85154f6
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-11 09:38:54 +0000

    Remove href attributes from navlinks in navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,8 +15,9 @@ const NavBar = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link>Home</Nav.Link>
+                        <Nav.Link>Sign in</Nav.Link>
+                        <Nav.Link>Sign up</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
