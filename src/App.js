29a07718883b80fd797bf9027ef69c789ebc8304
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-12 08:46:45 +0000

    Add routes for sign in, sign up and home in app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
-import Container from 'react-bootstrap/Container';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    <div className={styles.App}>
-      <NavBar />
-      <Container className={styles.Main}>
-        <h1>Home</h1>
-        <h1>Sign in</h1>
-        <h1>Sign in</h1>
-      </Container>
-    </div>
-  );
+    return (
+        <div className={styles.App}>
+            <NavBar />
+            <Container className={styles.Main}>
+                <Switch>
+                    <Route exact path="/" render={() => <h1>Home</h1>} />
+                    <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+                    <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+                </Switch>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
