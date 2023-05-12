8c5de29efa1b22fccec039fb70f0a79c29f868cf
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-12 09:10:58 +0000

    Add active className to navlinks in navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,23 +2,45 @@ import React from "react";
 import Nav from "react-bootstrap/Nav";
 import NavbarBrand from "react-bootstrap/NavbarBrand";
 import Navbar from "react-bootstrap/Navbar";
-import NavLink from "react-bootstrap/NavLink";
+import { NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import logo from "../assets/logo.png"
-import styles from "../styles/NavBar.module.css"
-
+import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
-                <Navbar.Brand className={styles.NavBarBrandText}><img src={logo} alt="logo" /> BarBelles</Navbar.Brand>
+                <NavLink to="/">
+                    <Navbar.Brand className={styles.NavBarBrandText}>
+                        <img src={logo} alt="logo" /> BarBelles
+                    </Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
-                        <Nav.Link bsPrefix={styles.NavBarLink}><i className="fas fa-home"></i> Home</Nav.Link>
-                        <Nav.Link bsPrefix={styles.NavBarLink}><i className="fa-solid fa-arrow-right-to-bracket"></i> Sign in</Nav.Link>
-                        <Nav.Link bsPrefix={styles.NavBarLink}><i className="fa-solid fa-plus"></i> Sign up</Nav.Link>
+                        <NavLink
+                            to="/"
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                        >
+                            <i className="fas fa-home"></i> Home
+                        </NavLink>
+                        <NavLink
+                            to="/signin"
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                        >
+                            <i className="fa-solid fa-arrow-right-to-bracket"></i>{" "}
+                            Sign in
+                        </NavLink>
+                        <NavLink
+                            to="/signup"
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                        >
+                            <i className="fa-solid fa-plus"></i> Sign up
+                        </NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
