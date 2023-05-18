0a990d5c5277a382a5097b4a504aa0b310ce2f89
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-18 14:21:28 +0000

    Test api connection with template form

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://barbelles-api.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'https://barbelles-api.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
