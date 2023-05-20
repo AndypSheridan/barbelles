61ea7b41eb8c854ba933a55cc0c8cf6286d0c50d
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-20 17:08:31 +0000

    Add signinform.js and import css

---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import styles from "../../styles/SignInForm.module.css";
+
+const SignInForm = () => {
+    return (
+        <Container className={styles.signInBg}>
+            <Row>
+                <Col></Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default SignInForm;
