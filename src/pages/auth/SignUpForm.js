95f1c729d2b925d99f68a1eccd6fb9a4e53886f9
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-21 16:00:54 +0000

    Remove form text from bootstrap form,signupform.js

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -55,9 +55,6 @@ const SignUpForm = () => {
                                 value={username}
                                 onChange={handleChange}
                             />
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
                         </Form.Group>
 
                         <Form.Group controlId="formBasicPassword">
