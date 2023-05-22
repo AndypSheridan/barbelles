9bbf43d1296cb2e6c3b90d92268857832004ef90
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-22 10:44:39 +0000

    Add return statement in currentusercontext.js

--- src/App.js
+++ src/App.js
@@ -6,56 +6,22 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import HomePage from "./pages/HomePage";
 import SignInForm from "./pages/auth/SignInForm";
-import { createContext, useEffect, useState } from "react";
-import axios from "axios";
-
-export const CurrentUserContext = createContext();
-export const SetCurrentUserContext = createContext();
 
 function App() {
-    const [currentUser, setCurrentUser] = useState(null);
-
-    const handleMount = async () => {
-        try {
-            const { data } = await axios.get("dj-rest-auth/user");
-            setCurrentUser(data);
-        } catch (err) {
-            console.log(err);
-        }
-    };
-
-    useEffect(() => {
-        handleMount();
-    }, []);
-
     return (
-        <CurrentUserContext.Provider value={currentUser}>
-            <SetCurrentUserContext.Provider value={setCurrentUser}>
-                <div className={styles.App}>
-                    <NavBar />
-                    <Container className={styles.Main}>
-                        <Switch>
-                            <Route exact path="/" render={() => <HomePage />} />
-                            <Route
-                                exact
-                                path="/signin"
-                                render={() => <SignInForm />}
-                            />
-                            <Route
-                                exact
-                                path="/signup"
-                                render={() => <SignUpForm />}
-                            />
-                            <Route
-                                render={() => (
-                                    <p>Oh no, this page can't be found!!</p>
-                                )}
-                            />
-                        </Switch>
-                    </Container>
-                </div>
-            </SetCurrentUserContext.Provider>
-        </CurrentUserContext.Provider>
+        <div className={styles.App}>
+            <NavBar />
+            <Container className={styles.Main}>
+                <Switch>
+                    <Route exact path="/" render={() => <HomePage />} />
+                    <Route exact path="/signin" render={() => <SignInForm />} />
+                    <Route exact path="/signup" render={() => <SignUpForm />} />
+                    <Route
+                        render={() => <p>Oh no, this page can't be found!!</p>}
+                    />
+                </Switch>
+            </Container>
+        </div>
     );
 }
 
