5424ce98223a579b62e5680b8903f334f61c0d3b
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-22 09:08:59 +0000

    Add form to signinform.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import styles from '../styles/HomePage.module.css'
-import { Col, Row } from 'react-bootstrap'
+import React from "react";
+import styles from "../styles/HomePage.module.css";
+import { Col, Container, Row } from "react-bootstrap";
 
 const HomePage = () => {
-  return (
-   
-<Row className={styles.homeBackground}>
-  <Col>
-  </Col>
-</Row>
-  )
-}
+    return (
+        <Container className={styles.homeBackground}>
+            <Row>
+                <Col></Col>
+            </Row>
+        </Container>
+    );
+};
 
-export default HomePage
+export default HomePage;
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,12 +1,38 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { useHistory, Link } from "react-router-dom";
 import styles from "../../styles/SignInForm.module.css";
+import { Form, Button, Col, Row, Container, Alert } from "react-bootstrap";
+import axios from "axios";
 
 const SignInForm = () => {
     return (
         <Container className={styles.signInBg}>
+            <h1>Sign in</h1>
             <Row>
                 <Col>
+                    <Form className={`mx-auto ${styles.Form}`}>
+                        <Form.Group controlId="username">
+                            <Form.Label className="d-none">Username</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Username"
+                                name="username"
+                            />
+                        </Form.Group>
+
+                        <Form.Group controlId="password1">
+                            <Form.Label className="d-none">Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                name="password1"
+                            />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Sign up
+                        </Button>
+                    </Form>
                 </Col>
             </Row>
         </Container>
