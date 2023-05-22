9ceacb1d0611fe38b50bedb258c2689b0cd1f63d
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-22 10:13:01 +0000

    Define handlemount function in app.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
-import './api/axiosDefaults';
+import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import HomePage from "./pages/HomePage";
 import SignInForm from "./pages/auth/SignInForm";
 import { useState } from "react";
-
+import axios from "axios";
 
 function App() {
+    const [currentUser, setCurrentUser] = useState(null);
 
-    const[currentUser, setCurrentUser] = useState(null);
+    const handleMount = async () => {
+        try {
+            const { data } = await axios.get("dj-rest-auth/user");
+            setCurrentUser(data);
+        } catch {
+            console.log(err);
+        }
+    };
 
     return (
         <div className={styles.App}>
@@ -21,7 +29,9 @@ function App() {
                     <Route exact path="/" render={() => <HomePage />} />
                     <Route exact path="/signin" render={() => <SignInForm />} />
                     <Route exact path="/signup" render={() => <SignUpForm />} />
-                    <Route render={() => <p>Oh no, this page can't be found!!</p>} />
+                    <Route
+                        render={() => <p>Oh no, this page can't be found!!</p>}
+                    />
                 </Switch>
             </Container>
         </div>
