2b68da9a215f2a7f0a6051392bf0a80601cb7d70
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-22 12:55:04 +0000

    Add logged in navbar links in navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,17 +11,47 @@ import { useCurrentUser } from "../contexts/CurrentUserContext";
 const NavBar = () => {
     const currentUser = useCurrentUser();
 
-    const createPostIcon = (
+    // const createPostIcon = (
+    //     <NavLink
+    //         to="/posts/create"
+    //         className={styles.NavLink}
+    //         activeClassName={styles.Active}
+    //     >
+    //         <i className="fa-solid fa-arrow-up-from-bracket"></i> Share post
+    //     </NavLink>
+    // );
+
+    const signedInIcons = (
+        <>
         <NavLink
                 to="/posts/create"
                 className={styles.NavLink}
                 activeClassName={styles.Active}
             >
-                <i className="fa-solid fa-arrow-up-from-bracket"></i>Share post
+                <i className="fa-solid fa-arrow-up-from-bracket"></i> Share post
             </NavLink>
-    )
-
-    const signedInIcons = <>{currentUser?.username}</>
+            <NavLink
+                to="/feed"
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+            >
+                <i className="fa-solid fa-list"></i> Feed
+            </NavLink>
+            <NavLink
+                to="/liked"
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+            >
+                <i className="fa-solid fa-bookmark"></i> My stuff
+            </NavLink>
+            <NavLink
+                to="/signin"
+                className={styles.NavLink}
+            >
+                <i className="fa-solid fa-door-open"></i> Log out
+            </NavLink>
+        </>
+    );
     const signedOutIcons = (
         <>
             <NavLink
@@ -50,6 +80,9 @@ const NavBar = () => {
                         BarBelles
                     </Navbar.Brand>
                 </NavLink>
+
+                {/* {currentUser && createPostIcon} */}
+
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
