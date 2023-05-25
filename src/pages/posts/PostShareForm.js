6ed0efd9eb4cd7905370b75ba945c3af1fead1f4
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-25 10:54:58 +0000

    Add div to display postshareform text small screen

--- src/pages/posts/PostShareForm.js
+++ src/pages/posts/PostShareForm.js
@@ -7,7 +7,9 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import styles from "../../styles/PostShareEditForm.module.css";
 import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css"
 import Asset from "../../components/Asset";
+import { Image } from "react-bootstrap";
 
 const PostShareForm = () => {
     const [errors, setErrors] = useState();
@@ -72,13 +74,16 @@ const PostShareForm = () => {
                 <Col className="py-2 p-0 p-md-2" md={6} lg={6}>
                     <Container className="d-flex flex-column justify-content-center">
                         <Form.Group className="text-center">
-                            ( image ? ( 
+                            { image ? ( 
                                 <>
                                 <figure>
-
+                                    <Image className={appStyles.Image} src={image} rounded />
                                 </figure>
                                 <div>
-                                    
+                                    <Form.Label className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                                    htmlFor="image-upload">Change image
+
+                                    </Form.Label>
                                 </div>
                                 </>
                             ) : (
@@ -91,13 +96,14 @@ const PostShareForm = () => {
                                     message={"Upload your image here!"}
                                 />
                             </Form.Label>
-                            ) )
+                            ) }
                             <Form.File
                                 id="image-upload"
                                 accept="image/*"
                                 onChange={handleChangeImage}
                             />
                         </Form.Group>
+                        <div className="d-md-none">{textFields}</div>
                     </Container>
                 </Col>
                 <Col md={6} lg={6} className="d-none d-md-block p-0 p-md-2">
