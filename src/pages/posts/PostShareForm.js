a4dc828d1536a810e4fbbd8884fb3b9390959eaf
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-25 09:37:39 +0000

    Add react bootstrap imports in postshareform.js

--- src/pages/posts/PostShareForm.js
+++ src/pages/posts/PostShareForm.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 import UploadIcon from "../../assets/uploadIcon.png";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+
+
 
 const PostShareForm = () => {
     const [errors, setErrors] = useState();
 
     const textFields = (
-        <div></div>
+        <div>
+            <Button>
+
+            </Button>
+        </div>
     )
 
     return <div>PostShareForm</div>;
