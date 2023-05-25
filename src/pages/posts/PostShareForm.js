5ddb7f1606c6fcd30c788fb6956589691faec648
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-25 09:39:59 +0000

    Add empty arrow function in postshareform.js

--- src/pages/posts/PostShareForm.js
+++ src/pages/posts/PostShareForm.js
@@ -5,20 +5,17 @@ import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import styles from "../../styles/PostShareEditForm.module.css"
-import appStyles from "../../App.module.css"
-
+import styles from "../../styles/PostShareEditForm.module.css";
+import appStyles from "../../App.module.css";
 
 const PostShareForm = () => {
     const [errors, setErrors] = useState();
 
     const textFields = (
         <div>
-            <Button>
-
-            </Button>
+            <Button onClick={() => {}}></Button>
         </div>
-    )
+    );
 
     return <div>PostShareForm</div>;
 };
