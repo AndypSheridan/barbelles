ac3aec82b110af3f243cf031b3ae294bc554536a
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-26 08:23:36 +0000

    Import Styles from app.module.css

--- src/pages/posts/PostDetailPage.js
+++ src/pages/posts/PostDetailPage.js
@@ -4,6 +4,8 @@ import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 
+import appStyles from "../../App.module.css"
+
 const PostDetailPage = () => {
   return (
     <div>PostDetailPage</div>
