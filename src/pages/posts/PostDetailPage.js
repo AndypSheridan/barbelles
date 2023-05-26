5ebe5be72b4a78b1f81daeb4907e19cc6d54c266
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-26 08:23:01 +0000

    Add react bootstrap imports in postdetailpage.js

--- src/pages/posts/PostDetailPage.js
+++ src/pages/posts/PostDetailPage.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
 const PostDetailPage = () => {
   return (
     <div>PostDetailPage</div>
