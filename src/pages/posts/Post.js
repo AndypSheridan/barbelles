82fc5e6e4ced4edeead3c1dcec4af5c09642fbee
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-28 17:40:58 +0000

    Add comments fa icon in post.js

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -3,7 +3,7 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
-import styles from "../../styles/Post.module.css"
+import styles from "../../styles/Post.module.css";
 
 const Post = (props) => {
     const {
@@ -25,36 +25,65 @@ const Post = (props) => {
     const is_owner = currentUser?.username === owner;
 
     return (
-    <Card>
-        <Card.Body>
-            <Media className="align-items-center justify-content-between">
-                <Link to={`/profiles/${profile_id}`}>
-                    <Avatar src={profile_image} height={60} />
-                    {owner}
-                </Link>
-                <div className="d-flex align-items-center justify-content-between">
-                    <span>{updated_at}</span>
-                    {is_owner && postPage && "..."}
-                </div>
-            </Media>
-        </Card.Body>
-        <Link to={`/posts/${id}`}>
-        <Card.Img src={image} alt={title} />
-        </Link>
-        <Card.Body>
-            {title && <Card.Title className="text-center">{title}</Card.Title>}
-            {story && <Card.Text>{story}</Card.Text>}
-            <div className={styles.PostInteraction}>
-                {is_owner ? (
-                    <OverlayTrigger placement="top" overlay={<Tooltip>You can't like your own post!</Tooltip>}>
-                        <i className="far fa-heart" />
-                    </OverlayTrigger>
-                ) : like_id ? (
-                    <span onClick={() => {}}></span>
+        <Card>
+            <Card.Body>
+                <Media className="align-items-center justify-content-between">
+                    <Link to={`/profiles/${profile_id}`}>
+                        <Avatar src={profile_image} height={60} />
+                        {owner}
+                    </Link>
+                    <div className="d-flex align-items-center justify-content-between">
+                        <span>{updated_at}</span>
+                        {is_owner && postPage && "..."}
+                    </div>
+                </Media>
+            </Card.Body>
+            <Link to={`/posts/${id}`}>
+                <Card.Img src={image} alt={title} />
+            </Link>
+            <Card.Body>
+                {title && (
+                    <Card.Title className="text-center">{title}</Card.Title>
                 )}
-            </div>
-        </Card.Body>
-    </Card>
+                {story && <Card.Text>{story}</Card.Text>}
+                <div className={styles.PostInteraction}>
+                    {is_owner ? (
+                        <OverlayTrigger
+                            placement="top"
+                            overlay={
+                                <Tooltip>You can't like your own post!</Tooltip>
+                            }
+                        >
+                            <i className="far fa-heart" />
+                        </OverlayTrigger>
+                    ) : like_id ? (
+                        <span onClick={() => {}}>
+                            <i className="fas fa-heart" />
+                        </span>
+                    ) : currentUser ? (
+                        <span onClick={() => {}}>
+                            <i className="far fa-heart" />
+                        </span>
+                    ) : (
+                        <OverlayTrigger
+                            placement="top"
+                            overlay={
+                                <Tooltip>
+                                    Please log in or sign up to like a post!
+                                </Tooltip>
+                            }
+                        >
+                            <i className="far fa-heart" />
+                        </OverlayTrigger>
+                    )}
+                    {likes_count}
+                    <Link to={`/posts/${id}`}>
+                        <i className="far fa-comments" />
+                    </Link>
+                    {comments_count}
+                </div>
+            </Card.Body>
+        </Card>
     );
 };
 
