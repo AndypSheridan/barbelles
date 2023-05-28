29ae30088b977b9bd91fa3a80a949a1e0ea72d82
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-28 16:05:24 +0000

    Import axiosreq in postdetailpage.js

--- src/pages/posts/PostDetailPage.js
+++ src/pages/posts/PostDetailPage.js
@@ -7,20 +7,21 @@ import Col from "react-bootstrap/Col";
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostDetailPage.module.css";
 import { useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
 const PostDetailPage = () => {
     const { id } = useParams();
     const [post, setPost] = useState({ results: [] });
 
-    useEffect(()=>{
-      const handleMount = async () => {
-        try{
-          
-        } catch(err){
-          
-        }
-      }
-    })
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const [{ data: post }] = await Promise.all([axiosReq.get(``)]);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+    });
 
     return (
         <Container className="h-100">
