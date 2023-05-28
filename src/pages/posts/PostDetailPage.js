b6c33cb59261e1a9c9779718eb7c7342ea0bdfc3
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-28 15:57:29 +0000

    Adjust padding in postdetailpage.module.css

--- src/pages/posts/PostDetailPage.js
+++ src/pages/posts/PostDetailPage.js
@@ -1,31 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-import appStyles from "../../App.module.css"
-import styles from "../../styles/PostDetailPage.module.css"
-import { useParams } from 'react-router-dom';
+import appStyles from "../../App.module.css";
+import styles from "../../styles/PostDetailPage.module.css";
+import { useParams } from "react-router-dom";
 
 const PostDetailPage = () => {
+    const { id } = useParams();
 
-    const {id} = useParams
+    return (
+        <Container className="h-100">
+            <Row className={styles.Row}>
+                <Col>
+                    <p>Placeholder text</p>
+                    <p>Post component</p>
+                    <Container>Comments</Container>
+                </Col>
+                <Col>
+                <p>More placeholder text</p>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
 
-  return (
-    <Container className='h-100'>
-        <Row className={styles.Row}>
-            <Col>
-            <p>Placeholder text</p>
-            <p>Post component</p>
-            <Container>
-                Comments
-            </Container>
-
-            </Col>
-        </Row>
-    </Container>
-  )
-}
-
-export default PostDetailPage
+export default PostDetailPage;
