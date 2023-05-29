09f132d213d1e0eb81bd212c2ab0ddd0f1a7061c
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-29 16:33:13 +0000

    Add style imports in postsfeedpage.js

--- src/pages/posts/PostsFeedPage.js
+++ src/pages/posts/PostsFeedPage.js
@@ -5,6 +5,9 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 
+import appStyles from "../../App.module.css"
+import styles from "../../styles/PostsFeedPage.module.css"
+
 const PostsFeedPage = () => {
     return <div>PostsFeedPage</div>;
 };
