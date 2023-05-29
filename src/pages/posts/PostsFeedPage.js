05bab36b4e5822e2463b154319e901ea09815872
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-29 16:32:15 +0000

    Add react-bootstrap imports in postsfeedspage.js

---
+++ src/pages/posts/PostsFeedPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+
+const PostsFeedPage = () => {
+    return <div>PostsFeedPage</div>;
+};
+
+export default PostsFeedPage;
