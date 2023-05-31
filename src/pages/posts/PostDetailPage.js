858459bc4f63d62dcc717b70a208a132fc1eea6c
AndypSheridan <andy.p.sheridan@gmail.com>
2023-05-31 16:07:02 +0000

    Add currentuser, profile image, comments in pdp.js

--- src/pages/posts/PostDetailPage.js
+++ src/pages/posts/PostDetailPage.js
@@ -1,33 +1,37 @@
 import React, { useEffect, useState } from "react";
-
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import styles from "../../styles/PostDetailPage.module.css";
+import CommentShareForm from "../comments/CommentShareForm";
+import { axiosReq } from "../../api/axiosDefaults";
 import Container from "react-bootstrap/Container";
+import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
-import appStyles from "../../App.module.css";
-import styles from "../../styles/PostDetailPage.module.css";
-import { useParams } from "react-router-dom";
-import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
-import CommentShareForm from "../comments/CommentShareForm";
 
 const PostDetailPage = () => {
     const { id } = useParams();
     const [post, setPost] = useState({ results: [] });
 
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState({ results: [] });
+
     useEffect(() => {
         const handleMount = async () => {
             try {
-                const [{ data: post }] = await Promise.all([axiosReq.get(`/posts/${id}`),
-              ])
-              setPost({results: [post]})
-              console.log(post)
+                const [{ data: post }] = await Promise.all([
+                    axiosReq.get(`/posts/${id}`),
+                ]);
+                setPost({ results: [post] });
+                console.log(post);
             } catch (err) {
                 console.log(err);
             }
         };
 
-    handleMount()
+        handleMount();
     }, [id]);
 
     return (
