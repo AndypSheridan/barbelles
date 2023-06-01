3ce436e77078d655f25a012424704c30a9e8ae98
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-01 13:11:58 +0000

    Add layout in ProfilePage.js

--- src/profiles/ProfilePage.js
+++ src/profiles/ProfilePage.js
@@ -1,15 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 import Container from "react-bootstrap/Container";
+import TopProfiles from "./TopProfiles";
 import Asset from "../components/Asset";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import TopProfiles from "./TopProfiles";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const ProfilePage = () => {
 
     const [hasLoaded, setHasLoaded] = useState(false);
     const currentUser = useCurrentUser();
+
+    useEffect(() => {
+        setHasLoaded(true);
+    }, [])
+
+    const mainProfile = (
+        <>
+        <Row>
+            <Col>
+            </Col>
+            <Col>
+            </Col>
+            <Col>
+            </Col><Col>
+            </Col>
+        </Row>
+        </>
+    )
 
     return (
         <Container>
