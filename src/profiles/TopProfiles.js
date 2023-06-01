82f80bacf2b7f14fd8d2e441e32a5f8373d53e63
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-01 11:15:38 +0000

    Import useState in topprofiles.js

--- src/profiles/TopProfiles.js
+++ src/profiles/TopProfiles.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import appStyles from "../App.module.css";
 import Container from "react-bootstrap/Container";
 
 const TopProfiles = () => {
+
+    const [profilesData, setProfileData] = useState
+
     return <Container className={appStyles.Content}>
         <p>Top profiles</p>
     </Container>;
