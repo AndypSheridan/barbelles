f96920a6f0a7c11d427b8d88e6d64a17cf7d7fc5
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-01 11:19:04 +0000

    Import useeffect and axiosreq in topprofiles.js

--- src/profiles/TopProfiles.js
+++ src/profiles/TopProfiles.js
@@ -1,14 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import appStyles from "../App.module.css";
 import Container from "react-bootstrap/Container";
+import { axiosReq } from "../api/axiosDefaults";
 
 const TopProfiles = () => {
+    const [profileData, setProfileData] = useState({
+        pageProfile: { results: [] },
+        topProfiles: { results: [] },
+    });
 
-    const [profilesData, setProfileData] = useState
+    const { topProfiles } = profileData;
 
-    return <Container className={appStyles.Content}>
-        <p>Top profiles</p>
-    </Container>;
+    useEffect(() => {
+        const handleMount = async () => {
+            try{
+                const {data} = await axiosReq
+            } catch(err) {
+
+            }
+        }
+    })
+
+    return (
+        <Container className={appStyles.Content}>
+            <p>Top profiles</p>
+        </Container>
+    );
 };
 
 export default TopProfiles;
