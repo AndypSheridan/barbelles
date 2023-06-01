4a56b83a2c5209c6a6c7b2d0d2de8e0a2443ca44
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-01 09:27:25 +0000

    Import, call usecurrentuser hook in postcomment.js

--- src/pages/comments/PostComment.js
+++ src/pages/comments/PostComment.js
@@ -1,26 +1,30 @@
 import React from "react";
 import styles from "../../styles/PostComment.module.css";
-import { Media } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
+import Media from "react-bootstrap/Media";
+import { Link } from "react-router-dom";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const PostComment = (props) => {
     const { profile_id, profile_image, owner, updated_at, content } = props;
 
-    return(
-    <div>
-        <hr />
-        <Media>
-            <Link to={`/profiles/${profile_id}`}>
-                <Avatar src={profile_image} />
-            </Link>
-            <Media.Body className="align-self-center ml-2">
-                <span className={styles.Owner}>{owner}</span>
-                <span className={styles.Date}>{updated_at}</span>
-                <p>{content}</p>
-            </Media.Body>
-        </Media>
-    </div>)
+    const currentUser = useCurrentUser()
+
+    return (
+        <div>
+            <hr />
+            <Media>
+                <Link to={`/profiles/${profile_id}`}>
+                    <Avatar src={profile_image} />
+                </Link>
+                <Media.Body className="align-self-center ml-2">
+                    <span className={styles.Owner}>{owner}</span>
+                    <span className={styles.Date}>{updated_at}</span>
+                    <p>{content}</p>
+                </Media.Body>
+            </Media>
+        </div>
+    );
 };
 
 export default PostComment;
