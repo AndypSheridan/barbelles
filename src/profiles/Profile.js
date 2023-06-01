cd83eb0f395aab0895c38f87d052be60e7128c4d
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-01 12:21:27 +0000

    Set Profile.js buttons to empty arrow functions

--- src/profiles/Profile.js
+++ src/profiles/Profile.js
@@ -1,8 +1,10 @@
 import React from "react";
-import styles from "../styles/Profile.module.css";
-import btnStyles from "../styles/Button.module.css";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
+import btnStyles from "../styles/Button.module.css";
+import styles from "../styles/Profile.module.css";
 import Avatar from "../components/Avatar";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 const Profile = (props) => {
     const { profile, mobile, imageSize = 60 } = props;
@@ -14,8 +16,20 @@ const Profile = (props) => {
     return <div className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}>
         <div>
             <Link to={`/profiles/${id}`} className="align-self-center">
-                <Avatar
+                <Avatar src={image} height={imageSize} />
             </Link>
+        </div>
+        <div className={`${styles.WordBreak} mx-2`}>
+        <strong>{owner}</strong>
+        </div>
+        <div className={`text-right ${!mobile && 'ml-auto'}`}>
+            {!mobile && currentUser && !is_owner && (
+                following_id ? (
+                    <Button onClick={()=>{}}>Unfollow</Button>
+                ) : (
+                    <Button onClick={()=>{}}>Follow</Button>
+                )
+            )}
         </div>
     </div>;
 };
