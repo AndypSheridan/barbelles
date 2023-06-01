fe4ccbe8d704068e909a00d79e9384ef29e16709
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-01 13:03:18 +0000

    Add imports in ProfilePage.js

--- src/profiles/ProfilePage.js
+++ src/profiles/ProfilePage.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Asset from '../components/Asset'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <Container>
+        <R
+    </Container>
   )
 }
 
