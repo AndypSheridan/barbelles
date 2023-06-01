02afd822488414fdac9b7f03fe74ef0b61db8b60
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-01 09:38:55 +0000

    Add componentdropdown, pass in handleedit/delete

--- src/pages/comments/PostComment.js
+++ src/pages/comments/PostComment.js
@@ -4,11 +4,13 @@ import Avatar from "../../components/Avatar";
 import Media from "react-bootstrap/Media";
 import { Link } from "react-router-dom";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { ComponentDropDown } from "../../components/ComponentDropDown";
 
 const PostComment = (props) => {
     const { profile_id, profile_image, owner, updated_at, content } = props;
 
-    const currentUser = useCurrentUser()
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
 
     return (
         <div>
@@ -22,6 +24,9 @@ const PostComment = (props) => {
                     <span className={styles.Date}>{updated_at}</span>
                     <p>{content}</p>
                 </Media.Body>
+                (is_owner && (
+                    <ComponentDropDown handleCommentEdit={() => {}} handleCommentDelete={() => {}} />
+                ))
             </Media>
         </div>
     );
