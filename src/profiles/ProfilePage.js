a68d68518797ed420e71873d8e57031c43cfa993
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-01 13:06:30 +0000

    Add ternary to ProfilePage.js return statement

--- src/profiles/ProfilePage.js
+++ src/profiles/ProfilePage.js
@@ -1,21 +1,30 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container'
-import Asset from '../components/Asset'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import TopProfiles from './TopProfiles'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Asset from "../components/Asset";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import TopProfiles from "./TopProfiles";
 
 const ProfilePage = () => {
-  return (
-    <Container>
-        <Row>
-            <Col>
-            <TopProfiles
-            
-            </Col>
-        </Row>
-    </Container>
-  )
-}
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <TopProfiles mobile />
+                    <Container>
+                        {hasLoaded ? (
+                            <>
+                                {mainProfile}
+                                {mainProfilePosts}
+                            </>
+                        ) : (
+                            <Asset spinner />
+                        )}
+                    </Container>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
 
-export default ProfilePage
+export default ProfilePage;
