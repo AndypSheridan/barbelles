03f41921cf21c268fc3474d09e51bc04f1fe0c79
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-01 15:52:22 +0000

    Add handleFollow to button in profile.js

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -14,27 +14,34 @@ const Profile = (props) => {
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
 
-    const {handleFollow} = useSetProfileData
+    const { handleFollow } = useSetProfileData();
 
-    return <div className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}>
-        <div>
-            <Link to={`/profiles/${id}`} className="align-self-center">
-                <Avatar src={image} height={imageSize} />
-            </Link>
+    return (
+        <div
+            className={`my-3 d-flex align-items-center ${
+                mobile && "flex-column"
+            }`}
+        >
+            <div>
+                <Link to={`/profiles/${id}`} className="align-self-center">
+                    <Avatar src={image} height={imageSize} />
+                </Link>
+            </div>
+            <div className={`${styles.WordBreak} mx-2`}>
+                <strong>{owner}</strong>
+            </div>
+            <div className={`text-right ${!mobile && "ml-auto"}`}>
+                {!mobile &&
+                    currentUser &&
+                    !is_owner &&
+                    (following_id ? (
+                        <Button onClick={() => {}}>Unfollow</Button>
+                    ) : (
+                        <Button onClick={() => handleFollow(profile)}>Follow</Button>
+                    ))}
+            </div>
         </div>
-        <div className={`${styles.WordBreak} mx-2`}>
-        <strong>{owner}</strong>
-        </div>
-        <div className={`text-right ${!mobile && 'ml-auto'}`}>
-            {!mobile && currentUser && !is_owner && (
-                following_id ? (
-                    <Button onClick={()=>{}}>Unfollow</Button>
-                ) : (
-                    <Button onClick={()=>{}}>Follow</Button>
-                )
-            )}
-        </div>
-    </div>;
+    );
 };
 
 export default Profile;
