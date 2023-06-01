96ad0ce4f30a946125d90f615a1c4a08b5ec68b4
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-01 08:44:37 +0000

    Add axiosreq to fetch comments,postdetailpage.js

--- src/pages/posts/PostDetailPage.js
+++ src/pages/posts/PostDetailPage.js
@@ -21,11 +21,12 @@ const PostDetailPage = () => {
     useEffect(() => {
         const handleMount = async () => {
             try {
-                const [{ data: post }] = await Promise.all([
+                const [{ data: post }, {data: comments} ] = await Promise.all([
                     axiosReq.get(`/posts/${id}`),
+                    axiosReq.get(`/comments/?post=${id}`)
                 ]);
                 setPost({ results: [post] });
-                console.log(post);
+                setComments(comments)
             } catch (err) {
                 console.log(err);
             }
