f4f9d48489426ab155c8839422bbe73d07cad1c6
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-01 15:31:27 +0000

    Add missing curly brackets in ProfilePage.js

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -17,6 +17,7 @@ import { Button, Image } from "react-bootstrap";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Post from "../posts/Post";
 import { fetchMoreData } from "../../utils/utils";
+import NoSearchResults from "../../assets/nosearchresults.png";
 
 const ProfilePage = () => {
     const [hasLoaded, setHasLoaded] = useState(false);
@@ -27,15 +28,16 @@ const ProfilePage = () => {
     const [profile] = pageProfile.results;
     const is_owner = currentUser?.username === profile?.owner;
 
-    const [profilePosts, setProfilePosts] = useState({ results: []});
+    const [profilePosts, setProfilePosts] = useState({ results: [] });
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const [{ data: pageProfile, data: profilePosts }] = await Promise.all([
-                    axiosReq.get(`/profiles/${id}/`),
-                    axiosReq.get(`/posts/?owner__profile=${id}`)
-                ]);
+                const [{data: pageProfile}, {data: profilePosts} ] =
+                    await Promise.all([
+                        axiosReq.get(`/profiles/${id}/`),
+                        axiosReq.get(`/posts/?owner__profile=${id}`),
+                    ]);
                 setProfileData((prevState) => ({
                     ...prevState,
                     pageProfile: { results: [pageProfile] },
@@ -63,34 +65,30 @@ const ProfilePage = () => {
                     <h4>{profile?.owner}</h4>
                     <Row className="justify-content-center no-gutters">
                         <Col xs={3} className="my-2">
-                            <div>
-                                {profile?.posts_count}
-                            </div>
+                            <div>{profile?.posts_count}</div>
                             <div>Posts</div>
                         </Col>
                         <Col xs={3} className="my-2">
-                            <div>
-                                {profile?.followers_count}
-                            </div>
+                            <div>{profile?.followers_count}</div>
                             <div>Followers</div>
                         </Col>
                         <Col xs={3} className="my-2">
-                            <div>
-                                {profile?.following_count}
-                            </div>
+                            <div>{profile?.following_count}</div>
                             <div>Following</div>
                         </Col>
                     </Row>
                 </Col>
                 <Col className="text-lg-right" lg={3}>
-                    {currentUser && !is_owner && (profile?.following_id ? (
-                        <Button onClick={() => {}}>Unfollow</Button>
-                    ) : (
-                        <Button onClick={()=>{}}>Follow</Button>
-                    ))}
+                    {currentUser &&
+                        !is_owner &&
+                        (profile?.following_id ? (
+                            <Button onClick={() => {}}>Unfollow</Button>
+                        ) : (
+                            <Button onClick={() => {}}>Follow</Button>
+                        ))}
                 </Col>
                 <Col className="p-3">
-                    { profile?.bio && (<Col className="p-3">{profile.bio}</Col>)}
+                    {profile?.bio && <Col className="p-3">{profile.bio}</Col>}
                 </Col>
             </Row>
         </>
@@ -99,21 +97,27 @@ const ProfilePage = () => {
     const mainProfilePosts = (
         <>
             <hr />
-            <p className="text-center">{profile?.owner}'s' posts</p>
+            <p className="text-center">{profile?.owner}'s posts</p>
             <hr />
             {profilePosts.results.length ? (
-                <InfiniteScroll children={profilePosts.results.map((post) => (
-                    <Post key={post.id} {...post} setPosts={setProfilePosts} />
-                ))}
-                dataLength={profilePosts.results.length}
-                loader={<Asset spinner />}
-                hasMore={!!profilePosts.next}
-                next={() => fetchMoreData(profilePosts, setProfilePosts)}
-                >
-
-                </InfiniteScroll>
+                <InfiniteScroll
+                    children={profilePosts.results.map((post) => (
+                        <Post
+                            key={post.id}
+                            {...post}
+                            setPosts={setProfilePosts}
+                        />
+                    ))}
+                    dataLength={profilePosts.results.length}
+                    loader={<Asset spinner />}
+                    hasMore={!!profilePosts.next}
+                    next={() => fetchMoreData(profilePosts, setProfilePosts)}
+                />
             ) : (
-
+                <Asset
+                    src={NoSearchResults}
+                    message={`No results found, ${profile?.owner} hasn't posted yet.`}
+                />
             )}
         </>
     );
