fe6dce2b5b8652e4fb5d30a6ccdeeb54c93a7a63
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-01 10:17:48 +0000

    Add layout in commenteditform.js

--- src/pages/comments/CommentEditForm.js
+++ src/pages/comments/CommentEditForm.js
@@ -1,11 +1,30 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 const CommentEditForm = () => {
     const { id, content, setShowEditForm, setComments } = props;
 
     const [formContent, setFormContent] = useState(content);
 
-    return <div>CommentEditForm</div>;
+    return (
+        <Form>
+            <Form.Group>
+                <Form.Control />
+            </Form.Group>
+            <Form.Group>
+                <Form.Control />
+            </Form.Group>
+            <div>
+            <button>
+
+            </button>
+            <button>
+                
+            </button>
+            </div>
+        </Form>
+    )
+    
 };
 
 export default CommentEditForm;
