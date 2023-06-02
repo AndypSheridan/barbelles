9d5cbc62d7b44ad3726ab47a0c1fdeb1c3502a7f
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-02 11:25:29 +0000

    Add handleUnfollow in ProfileDataContext.js

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -82,7 +82,7 @@ export const ProfileDataProvider = ({ children }) => {
     return (
         <ProfileDataContext.Provider value={profileData}>
             <SetProfileDataContext.Provider
-                value={{ setProfileData, handleFollow }}
+                value={{ setProfileData, handleFollow, handleUnfollow }}
             >
                 {children}
             </SetProfileDataContext.Provider>
