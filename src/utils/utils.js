4b3f9888dcd1423c84a4eb58bd001fc74c665c1b
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-03 09:12:21 +0000

    Add removeTokenTimestamp function in utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -36,18 +36,28 @@ export const followHelper = (profile, clickedProfile, following_id) => {
 
 export const unfollowHelper = (profile, clickedProfile) => {
     return profile.id === clickedProfile.id
-    ? {
-        ...profile,
-        followers_count: profile.followers_count -1,
-        following_id: null,
-    }
-    : profile.is_owner
-    ? {
-        ...profile, following_count: profile.following_count -1
-    }
-    : profile;
-}
+        ? {
+              ...profile,
+              followers_count: profile.followers_count - 1,
+              following_id: null,
+          }
+        : profile.is_owner
+        ? {
+              ...profile,
+              following_count: profile.following_count - 1,
+          }
+        : profile;
+};
 
 export const setTokenTimestamp = (data) => {
-    const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp
+    const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
+    localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
+};
+
+export const shouldRefreshToken = () => {
+    return !!localStorage.getItem("refreshTokenTimestamp")
+}
+
+export const removeTokenTimestamp = () => {
+    localStorage.removeItem("refreshTokenTimestamp")
 }
