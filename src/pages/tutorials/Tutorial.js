d89a62b443e51cafb5c159388aa98d2c92beb2f6
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-03 12:44:12 +0000

    Add iframe and props in tutorial.js

--- src/pages/tutorials/Tutorial.js
+++ src/pages/tutorials/Tutorial.js
@@ -1,7 +1,10 @@
 import React from "react";
 import styles from "../../styles/Tutorial.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import { Card, Media } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import Media from "react-bootstrap/Media";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
 
 const Tutorial = (props) => {
     const {
@@ -16,19 +19,35 @@ const Tutorial = (props) => {
         summary,
         video,
         updated_at,
+        tutorialDetailPage,
     } = props;
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
 
-    return <Card className={styles.Tutorial}>
-        <Card.Body>
-            <Media className="align-items-center justify-content-between">
-
-            </Media>
-        </Card.Body>
-
-    </Card>
+    return (
+        <Card className={styles.Tutorial}>
+            <Card.Body>
+                <Media className="align-items-center justify-content-between">
+                    <Link to={`/profiles/${profile_id}`}>
+                        <Avatar src={profile_image} height={60} />
+                        {owner}
+                    </Link>
+                    <div className="d-flex align-items-center">
+                        <span>{updated_at}</span>
+                        {is_owner && tutorialDetailPage && "..."}
+                    </div>
+                </Media>
+            </Card.Body>
+            <div className="embed-responsive embed-responsive-16by9">
+                <iframe
+                    src={video}
+                    title="Tutorial"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                ></iframe>
+            </div>
+        </Card>
+    );
 };
 
 export default Tutorial;
