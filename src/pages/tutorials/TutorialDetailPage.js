927fe4f1e4b04ecb6ebbab583785652bc6bb04f9
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-03 11:30:03 +0000

    Import useEffect in tutdetailpage.js

--- src/pages/tutorials/TutorialDetailPage.js
+++ src/pages/tutorials/TutorialDetailPage.js
@@ -1,29 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-import styles from "../../styles/TutorialDetailPage.module.css"
+import styles from "../../styles/TutorialDetailPage.module.css";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
 
 const TutorialDetailPage = () => {
+    const { id } = useParams();
+    const [tutorial, setTutorial] = useState({ results: [] });
 
-    const { id } =useParams();
-    const [tutorial, setTutorial] = useState()
+    useEffect()
 
     return (
         <Container className="h-100">
-        <Row className={`${styles.Row}`}>
-            <Col className="py-2 p-0 p-lg-2" lg={8}>
-                <p>Placeholder 1</p>
-                <p>Tutorial component</p>
-                <Container className={appStyles.Content}>Comments</Container>
-            </Col>
-            <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-                Placeholder 2
-            </Col>
-        </Row>
+            <Row className={`${styles.Row}`}>
+                <Col className="py-2 p-0 p-lg-2" lg={8}>
+                    <p>Placeholder 1</p>
+                    <p>Tutorial component</p>
+                    <Container className={appStyles.Content}>
+                        Comments
+                    </Container>
+                </Col>
+                <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+                    Placeholder 2
+                </Col>
+            </Row>
         </Container>
     );
 };
