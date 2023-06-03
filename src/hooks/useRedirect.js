10f3a8e673815a79990461cf85d3103a3ec535fc
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-03 08:29:51 +0000

    Import axios in useredirect.js

--- src/hooks/useRedirect.js
+++ src/hooks/useRedirect.js
@@ -1,5 +1,17 @@
 import { useHistory } from "react-router-dom"
+import { useEffect } from "react";
+import axios from "axios";
 
-export const useRedirect = (userAuthStatus) => [
-    const history - useHistory
-]
+export const useRedirect = (userAuthStatus) => {
+    const history = useHistory();
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                await axios.post('/dj-rest-auth/token/refresh/')
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    })
+
+}
