e4a940a692c8c044e8c61003b153504732285c57
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-03 08:35:26 +0000

    Call handleMount, add dependencies> useredirect.js

--- src/hooks/useRedirect.js
+++ src/hooks/useRedirect.js
@@ -8,10 +8,16 @@ export const useRedirect = (userAuthStatus) => {
         const handleMount = async () => {
             try {
                 await axios.post('/dj-rest-auth/token/refresh/')
+                if (userAuthStatus === "loggedIn"){
+                    history.push("/")
+                }
             } catch (err) {
-                console.log(err)
+                if (userAuthStatus === "loggedOut"){
+                    history.push("/")
+                }
             }
         }
-    })
+        handleMount();
+    }, [history, userAuthStatus])
 
 }
