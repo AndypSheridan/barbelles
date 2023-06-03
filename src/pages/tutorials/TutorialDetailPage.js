5d2e4f67bec455f9c7da4df3afd15fdb7262077f
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-03 11:10:25 +0000

    Add basic layout to tutorial detail page

---
+++ src/pages/tutorials/TutorialDetailPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import appStyles from "../../App.module.css";
+
+const TutorialPage = () => {
+    return (
+        <Row className="h-100">
+            <Col className="py-2 p-0 p-lg-2" lg={8}>
+                <p>Placeholder 1</p>
+                <p>Tutorial component</p>
+                <Container className={appStyles.Content}>Comments</Container>
+            </Col>
+            <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+                Placeholder 2
+            </Col>
+        </Row>
+    );
+};
+
+export default TutorialPage;
