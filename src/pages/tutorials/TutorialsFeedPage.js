fd45d0e02a81ecdedad0c13a5236f4f058ac070b
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-04 11:52:13 +0000

    Import useeffect hook in TutorialsFeedPage.js

--- src/pages/tutorials/TutorialsFeedPage.js
+++ src/pages/tutorials/TutorialsFeedPage.js
@@ -1,24 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import styles from "../../styles/TutorialsFeedPage.module.css";
 import { Col, Row } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
 
-const TutorialsFeedPage = ({message, filter=""}) => {
-
-    const [tutorials, setTutorials] = useState({ results: []});
+const TutorialsFeedPage = ({ message, filter = "" }) => {
+    const [tutorials, setTutorials] = useState({ results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
-    const {pathname} = useLocation();
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        const fet
+    })
 
     return (
         <Container className={`${styles.Container} h-100`}>
             <Row>
                 <Col lg={8} className="p-0">
-                <p>PLaceholder 1</p>
-                <p>Tutorials</p>
+                    <p>PLaceholder 1</p>
+                    <p>Tutorials</p>
                 </Col>
                 <Col lg={4} className="d-lg-block d-none p-0">
-                <p>Placeholder 2</p>
+                    <p>Placeholder 2</p>
                 </Col>
             </Row>
             <div>TutorialsFeedPage</div>
