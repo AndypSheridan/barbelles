4115ce0dbaa49bfd29adcfa0da93bd5efbe751ce
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-06 08:29:19 +0000

    Add margin bottom to post card in Post.js

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -82,7 +82,7 @@ const Post = (props) => {
     };
 
     return (
-        <Card>
+        <Card className="mb-5">
             <Card.Body>
                 <Media className="align-items-center justify-content-between">
                     <Link to={`/profiles/${profile_id}`}>
