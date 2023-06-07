a4e29cde97e409ad1d31ab4b45f67e556a013d49
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-07 11:40:17 +0000

    Import screen in navbar.test.js

--- src/components/__tests__/NavBar.test.js
+++ src/components/__tests__/NavBar.test.js
@@ -1,9 +1,11 @@
-import { render } from "@testing-library/react"
-import { BrowserRouter, Router } from "react-router-dom"
-import NavBar from "../NavBar"
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import NavBar from "../NavBar";
 
 test("renders navbar component", () => {
-    render(<Router>
-        <NavBar />
-    </Router>)
-})
+    render(
+        <Router>
+            <NavBar />
+        </Router>
+    );
+});
