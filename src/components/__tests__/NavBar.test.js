6e7d6b72231900e84a69dcb7d2ccc2f99fac108b
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-07 12:16:08 +0000

    Import fireEvent in NavBar.test.js

--- src/components/__tests__/NavBar.test.js
+++ src/components/__tests__/NavBar.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "../NavBar";
 import { CurrentUserProvider } from "../../contexts/CurrentUserContext";
@@ -15,7 +15,7 @@ test("renders navbar component", () => {
     expect(signUpLink).toBeInTheDocument();
 });
 
-test("renders posts link for a logged in user", async () => {
+test("renders about link for a logged in user", async () => {
     render(
         <Router>
             <CurrentUserProvider>
@@ -25,4 +25,29 @@ test("renders posts link for a logged in user", async () => {
     );
 
     const aboutLink = await screen.findByText("About");
+});
+
+test("renders tutorials link for a logged in user", async () => {
+    render(
+        <Router>
+            <CurrentUserProvider>
+                <NavBar />
+            </CurrentUserProvider>
+        </Router>
+    );
+
+    const tutorialLink = await screen.findByText("Tutorials");
+    expect(tutorialLink).toBeInTheDocument();
+});
+
+test("renders signup button when user logs out", async () => {
+    render(
+        <Router>
+            <CurrentUserProvider>
+                <NavBar />
+            </CurrentUserProvider>
+        </Router>
+    );
+
+    const signOutLink = await screen.findByRole('button', {name: "Sign out"})
 });
