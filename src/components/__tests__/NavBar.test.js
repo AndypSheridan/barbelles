5194c30c0ad4836eb1a07da38dcd16eb29f32d64
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-07 11:54:34 +0000

    Import currentuserprovider in navbar.test.js

--- src/components/__tests__/NavBar.test.js
+++ src/components/__tests__/NavBar.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "../NavBar";
+import { CurrentUserProvider } from "../../contexts/CurrentUserContext";
 
 test("renders navbar component", () => {
     render(
@@ -14,14 +15,12 @@ test("renders navbar component", () => {
     expect(signUpLink).toBeInTheDocument();
 });
 
-test("renders navbar component", () => {
+test("renders profile link for a logged in user", async () => {
     render(
         <Router>
-            <NavBar />
+            <CurrentUserProvider>
+                <NavBar />
+            </CurrentUserProvider>
         </Router>
     );
-
-    // screen.debug();
-    const signUpLink = screen.getByRole("link", { name: "Sign up" });
-    expect(signUpLink).toBeInTheDocument();
 });
