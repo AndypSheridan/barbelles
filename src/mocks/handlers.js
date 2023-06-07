5125d521dccd06dc85cb2849d4c8e30c3163b62d
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-07 11:20:18 +0000

    Add json user data to handlers.js

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,7 +1,20 @@
-import { rest } from "msw"
+import { rest } from "msw";
 
-const baseURL = 'https://barbelles-api.herokuapp.com/'
+const baseURL = "https://barbelles-api.herokuapp.com/";
 
 export const handlers = [
-    rest.get(`${baseURL}dj-rest/auth/user/`)
-]
+    rest.get(`${baseURL}dj-rest/auth/user/`, (req, res, ctx) => {
+        return res(
+            ctx.json({
+                pk: 2,
+                username: "testuser1",
+                email: "",
+                first_name: "",
+                last_name: "",
+                profile_id: 2,
+                profile_image:
+                    "https://res.cloudinary.com/dzvaxeems/image/upload/v1/media/../default_profile_sigwcy",
+            })
+        );
+    }),
+];
