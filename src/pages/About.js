1f50cb796d062f9de9e06e607201df4fb646ce97
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-09 09:42:00 +0000

    Add basic layout in About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import styles from "../styles/About.module.css"
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
-import Container from 'react-bootstrap/Container'
+import React from "react";
+import styles from "../styles/About.module.css";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 const About = () => {
-  return (
-    <Container className={styles.AboutContainer}>
-        <Row>
-            <Col>
-            Hello there
-            </Col>
-        </Row>
-    </Container>
-  )
-}
+    return (
+        <Container className={styles.AboutContainer}>
+            <h1 className={`${styles.AboutHeader} text-center`}>About our community</h1>
+            <Row className={`${styles.AboutRow1} justify-content-center`}>
+                <Col className={`${styles.AboutCol1}`} lg={6}></Col>
+                <Col className={`${styles.AboutCol1}`}lg={6}></Col>
+            </Row>
+        </Container>
+    );
+};
 
-export default About
+export default About;
