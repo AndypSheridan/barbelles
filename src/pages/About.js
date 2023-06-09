91974a20a96e289f47cf99d2c7691e7e445a9aaa
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-09 09:23:04 +0000

    Add react bootstrap imports in About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import styles from "../styles/About.module.css"
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
 
 const About = () => {
   return (
-    <div>About</div>
+    <Container className={styles.AboutContainer}>
+        <Row>
+            <Col>
+            Hello there
+            </Col>
+        </Row>
+    </Container>
   )
 }
 
