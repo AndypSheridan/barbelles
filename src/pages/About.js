93f461cb498827a02e92d506a565dc18cfbda694
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-09 11:04:49 +0000

    Add css for col, container in About.module.css

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,14 +3,17 @@ import styles from "../styles/About.module.css";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import communityImage from "../assets/community.jpg"
 
 const About = () => {
     return (
         <Container className={styles.AboutContainer}>
             <h1 className={`${styles.AboutHeader} text-center`}>About our community</h1>
-            <Row className={`${styles.AboutRow1} justify-content-center`}>
-                <Col className={`${styles.AboutCol1}`} lg={6}></Col>
-                <Col className={`${styles.AboutCol2}`}lg={6}></Col>
+            <Row className={`${styles.AboutRow1} mt-3`}>
+                <Col lg={6}> hi</Col>
+                <Col lg={6}>
+                    <img className={`${styles.AboutCol2Image} img-fluid`} src={communityImage} />
+                </Col>
             </Row>
         </Container>
     );
