e638fcbee98e0a840020d98df22d34e9665b1a49
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-14 15:42:52 +0000

    Add aria labels in sociallinks.js

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -4,11 +4,42 @@ const SocialLinks = () => {
     return (
         <div>
             <h3 className="my-3">Follow us</h3>
-            <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer" className="px-2"><i className="fa-brands fa-square-youtube fa-3x"></i></a>
-            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className="px-2"><i className="fa-brands fa-square-instagram fa-3x"></i></a>
-            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="px-2"><i className="fa-brands fa-square-facebook fa-3x"></i></a>
-            <a href="https://www.twitter.com/" target="_blank" rel="noopener noreferrer" className="px-2"><i className="fa-brands fa-square-twitter fa-3x"></i></a>
-
+            <a
+                href="https://www.youtube.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="px-2"
+                aria-label="youtube"
+            >
+                <i className="fa-brands fa-square-youtube fa-3x"></i>
+            </a>
+            <a
+                href="https://www.instagram.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="px-2"
+                aria-label="instagram"
+            >
+                <i className="fa-brands fa-square-instagram fa-3x"></i>
+            </a>
+            <a
+                href="https://www.facebook.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="px-2"
+                aria-label="facebook"
+            >
+                <i className="fa-brands fa-square-facebook fa-3x"></i>
+            </a>
+            <a
+                href="https://www.twitter.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="px-2"
+                aria-label="twitter"
+            >
+                <i className="fa-brands fa-square-twitter fa-3x"></i>
+            </a>
         </div>
     );
 };
---
+++ src/components/__tests__/SocialLinks.test.js
@@ -0,0 +1,44 @@
+import { render, screen } from "@testing-library/react";
+import SocialLinks from "../SocialLinks";
+
+
+describe('SocialLinks', () => {
+  it('renders social media links with correct URLs', () => {
+    render(<SocialLinks />);
+
+    const youtubeLink = screen.getByRole('link', { name: 'YouTube' });
+    expect(youtubeLink).toHaveAttribute('href', 'https://www.youtube.com/');
+
+    const instagramLink = screen.getByRole('link', { name: 'Instagram' });
+    expect(instagramLink).toHaveAttribute('href', 'https://www.instagram.com/');
+
+    const facebookLink = screen.getByRole('link', { name: 'Facebook' });
+    expect(facebookLink).toHaveAttribute('href', 'https://www.facebook.com/');
+
+    const twitterLink = screen.getByRole('link', { name: 'Twitter' });
+    expect(twitterLink).toHaveAttribute('href', 'https://www.twitter.com/');
+  });
+
+  it('renders social media icons', () => {
+    render(<SocialLinks />);
+
+    const youtubeIcon = screen.getByRole('img', { name: 'YouTube' });
+    expect(youtubeIcon).toBeInTheDocument();
+
+    const instagramIcon = screen.getByRole('img', { name: 'Instagram' });
+    expect(instagramIcon).toBeInTheDocument();
+
+    const facebookIcon = screen.getByRole('img', { name: 'Facebook' });
+    expect(facebookIcon).toBeInTheDocument();
+
+    const twitterIcon = screen.getByRole('img', { name: 'Twitter' });
+    expect(twitterIcon).toBeInTheDocument();
+  });
+
+  it('renders the "Follow us" heading', () => {
+    render(<SocialLinks />);
+
+    const heading = screen.getByRole('heading', { name: 'Follow us' });
+    expect(heading).toBeInTheDocument();
+  });
+});
