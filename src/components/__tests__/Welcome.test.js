f46165cc831adb58b225845e211bf1bf28eb6420
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-14 16:33:02 +0000

    Fix Welcome.test.js typo to pass test

--- src/components/__tests__/Welcome.test.js
+++ src/components/__tests__/Welcome.test.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Welcome from "../components/Welcome";
+import Welcome from "../Welcome";
 
 describe("Welcome Component", () => {
   it("renders the welcome message and description", () => {
     render(<Welcome />);
 
     // Assert the content
-    expect(screen.getByText("Welcome to Barbelles")).toBeInTheDocument();
+    expect(screen.getByText("Welcome to")).toBeInTheDocument();
     expect(
       screen.getByText("An inclusive online fitness community for women")
     ).toBeInTheDocument();
