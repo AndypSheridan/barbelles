132b37b0cdcfbb5eff1e91134f821ff784051763
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-15 16:22:15 +0000

    Import Button in PageNotFound.js

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import styles from "../styles/PageNotFound.module.css"
-
+import React from "react";
+import styles from "../styles/PageNotFound.module.css";
+import Container from "react-bootstrap/Container";
+import noResults from "../assets/nosearchresults.png"
+import Asset from "./Asset";
+import Button from "react-bootstrap/Button";
 
 const PageNotFound = () => {
-  return (
-    <div className={styles.Div}>PageNotFound</div>
-  )
-}
+    return <Container className={styles.Container}>
+        <Asset src={noResults} message={"Sorry, the page you're looking for has either moved or doesn't exist"}/>
+        <Button></Button>
+    </Container>;
+};
 
-export default PageNotFound
+export default PageNotFound;
