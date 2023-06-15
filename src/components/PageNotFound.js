d6496cf4fd1a67eb1b1bf772c7f6565c335d955c
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-15 16:34:47 +0000

    Import history, add to button onclick>notfound.js

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,15 +1,31 @@
 import React from "react";
 import styles from "../styles/PageNotFound.module.css";
 import Container from "react-bootstrap/Container";
-import noResults from "../assets/nosearchresults.png"
+import noResults from "../assets/nosearchresults.png";
 import Asset from "./Asset";
 import Button from "react-bootstrap/Button";
+import btnStyles from "../styles/Button.module.css";
+import { useHistory } from "react-router-dom";
 
 const PageNotFound = () => {
-    return <Container className={styles.Container}>
-        <Asset src={noResults} message={"Sorry, the page you're looking for has either moved or doesn't exist"}/>
-        <Button></Button>
-    </Container>;
+    const history = useHistory();
+
+    return (
+        <Container className={styles.Container}>
+            <Asset
+                src={noResults}
+                message={
+                    "Sorry, the page you're looking for has either moved or doesn't exist"
+                }
+            />
+            <Button
+                className={`${btnStyles.Pink} ${btnStyles.Button} ${styles.GoBack}`}
+                onClick={() => history.goBack()}
+            >
+                Go back
+            </Button>
+        </Container>
+    );
 };
 
 export default PageNotFound;
