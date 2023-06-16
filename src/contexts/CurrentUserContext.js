a39480963c8cc83edddd42f124421205562c07c6
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-16 19:06:37 +0000

    Change history direct to home > currentusercontext

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -35,7 +35,7 @@ export const CurrentUserProvider = ({ children }) => {
                     } catch (err) {
                         setCurrentUser((prevCurrentUser) => {
                             if (prevCurrentUser) {
-                                history.push("/signup");
+                                history.push("/");
                             }
                             return null;
                         });
@@ -59,7 +59,7 @@ export const CurrentUserProvider = ({ children }) => {
                     } catch (err) {
                         setCurrentUser((prevCurrentUser) => {
                             if (prevCurrentUser) {
-                                history.push("/signup");
+                                history.push("/");
                             }
                             return null;
                         });
