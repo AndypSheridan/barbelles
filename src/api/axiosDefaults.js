2677b9b24c1f5a11cefc9df90b4d2d716887a171
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-21 10:24:44 +0000

    Add docstring in axiosDefaults.js

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-
+/**
+ * Connects to API. Based on code from Code Institute 'Moments' walkthrough.
+ */
 axios.defaults.baseURL = 'https://barbelles-api.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
