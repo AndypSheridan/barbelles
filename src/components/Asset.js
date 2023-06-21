04405a4b2f30e87f97f80c94e0323586457ae2a2
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-21 10:27:32 +0000

    Add docstring in Asset.js

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -2,6 +2,9 @@ import React from "react";
 import styles from "../styles/Asset.module.css";
 import Spinner from "react-bootstrap/Spinner";
 
+/**
+ * Adapted from code provided in CI 'Moments' walkthrough.
+ */
 const Asset = ({ spinner, src, message }) => {
 	return (
 		<div className={`${styles.Asset} p-4`}>
