ae4401db4939c5b45eb36a16f1201c7a75268e7b
AndypSheridan <andy.p.sheridan@gmail.com>
2023-06-21 10:54:23 +0000

    Add docstring in useRedirect.js

--- src/hooks/useRedirect.js
+++ src/hooks/useRedirect.js
@@ -2,6 +2,11 @@ import { useHistory } from "react-router-dom";
 import { useEffect } from "react";
 import axios from "axios";
 
+/**
+ * Adapted from code provided in CI 'Moments' walkthrough.
+ * Checks logged-in status with refresh token.
+ * Redirects user to home page if not logged in.
+ */
 export const useRedirect = (userAuthStatus) => {
 	const history = useHistory();
 	useEffect(() => {
